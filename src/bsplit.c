3786f2ab847231ff60b285a13011c5da5ca8f6d7
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-10 19:58:40 -0400

    Update bsplit.c

--- src/bsplit.c
+++ src/bsplit.c
@@ -17,7 +17,7 @@
 void
 bsplit(pNode me, int n1, int n2, int minsize, int split_Rule, double alpha, int bucketnum, int bucketMax,
        double train_to_est_ratio)
-{Rprintf( "Program running...\n" );
+{
     int i, j, k;
     int kk;
     int nc;
@@ -48,7 +48,7 @@ bsplit(pNode me, int n1, int n2, int minsize, int split_Rule, double alpha, int 
         k = 0;
         for (j = n1; j < n2; j++) {
             kk = index[j];
-
+Rprintf( "hello" );
             /* x data not missing and wt > 0 */
             if(kk >= 0 && ct.wt[kk] > 0) { 
                 xtemp[k] = ct.xdata[i][kk];

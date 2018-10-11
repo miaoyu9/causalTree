23d7d0de6b8a8d53a58655179dc2f1aeb408ffa2
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-11 10:12:03 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -40,7 +40,7 @@ void
 CTss(int n, double *y[], double *value,  double *con_mean, double *tr_mean, 
      double *risk, double *wt, double *treatment, double max_y,
      double alpha, double train_to_est_ratio)
-{
+{Rprintf("CT_CTss\n");
     int i;
     double temp0 = 0., temp1 = 0., twt = 0.; /* sum of the weights */ 
     double ttreat = 0.;
@@ -72,7 +72,7 @@ CTss(int n, double *y[], double *value,  double *con_mean, double *tr_mean,
 void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, double *split, 
         int *csplit, double myrisk, double *wt, double *treatment, int minsize, double alpha,
         double train_to_est_ratio)
-{
+{Rprintf("CT\n");
     int i, j;
     double temp;
     double left_sum, right_sum;

b99b81f57707b5abf81022b74b3ce731b02ea9e7
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-10 23:11:52 -0400

    Update causalTree.c

--- src/causalTree.c
+++ src/causalTree.c
@@ -60,6 +60,7 @@ causalTree(SEXP ncat2, SEXP split_Rule2, SEXP bucketnum2, SEXP bucketMax2, SEXP 
         SEXP ymat2, SEXP xmat2, SEXP wt2, SEXP treatment2, SEXP ny2, SEXP cost2, 
         SEXP xvar2, SEXP split_alpha2, SEXP cv_alpha2, SEXP NumHonest2, SEXP gamma2)
 {
+           Rprintf("ERROR for buket!\n");
     pNode tree;          /* top node of the tree */
     char *errmsg;
     int i, j, k, n;

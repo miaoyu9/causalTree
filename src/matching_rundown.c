ec17a5f38635ad8038b7dbe60c953419d2530694
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-11 05:59:03 -0400

    Update matching_rundown.c

--- src/matching_rundown.c
+++ src/matching_rundown.c
@@ -9,7 +9,7 @@
 void
 matching_rundown(pNode tree, int obs, int neighbor, double *cp, double *xpred, 
                  double *xpred2, double *xtemp)
-{
+{Rprintf("matching_rundown\n");
     int i, obs2 = (obs < 0) ? -(1 + obs) : obs;
     pNode otree =  tree;
     pNode tree2 = tree;

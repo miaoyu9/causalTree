74681290b66ba144a57a9436299beb4c41e417a7
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-10 21:02:43 -0400

    Update causalTree.R

--- R/causalTree.R
+++ R/causalTree.R
@@ -95,7 +95,7 @@ causalTree <- function(formula, data, weights, treatment, subset,
 	                                       "fitD", "tstatsD", "user", "userD","policy","policyD"))
 	print(split.Rule.int)
 	print(split.Rule)
-	print(split.Rule)
+	
 	if (is.na(split.Rule.int)) stop("Invalid splitting rule.")
 	split.Rule <- c("TOT", "CT", "fit", "tstats", "TOTD", "CTD", "fitD", 
 	                "tstatsD", "user", "userD","policy","policyD")[split.Rule.int]

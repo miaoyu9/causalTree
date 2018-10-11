2ecf0769083c31799aff11bcc0edda098004fee3
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-11 01:06:44 -0400

    Update partition.c

--- src/partition.c
+++ src/partition.c
@@ -44,7 +44,7 @@ partition(int nodenum, pNode splitnode, double *sumrisk, int n1, int n2,
     //fprintf(fptr,"test print\n");
     //fclose(fptr);
     R_FlushConsole();
-    Rprintf("partition\n");
+    Rprintf("partition.c\n");
    //R_ShowMessage("R_show_message\n");
     
 #endif

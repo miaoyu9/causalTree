b12d97e78beedacebce876a70e4f8eb0e226ca4d
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-10 20:05:58 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -59,7 +59,7 @@ CTss(int n, double *y[], double *value,  double *con_mean, double *tr_mean,
     double con_sqr_sum = 0., tr_sqr_sum = 0.;
     
     for (i = 0; i < n; i++) {
-Rprintf("CT");  
+  
         temp1 += *y[i] * wt[i] * treatment[i];
         temp0 += *y[i] * wt[i] * (1 - treatment[i]);
         twt += wt[i];

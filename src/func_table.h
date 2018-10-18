796e7f831b94a96993c8aee3ef665e57f885977f
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-18 09:56:40 -0400

    Update func_table.h

--- src/func_table.h
+++ src/func_table.h
@@ -28,11 +28,15 @@ extern double totDpred(double *y, double wt, double treatment, double *yhat, dou
 extern int CTinit(int n, double *y[], int maxcat, char **error,
            int *size, int who, double *wt, double *treatment, int bucketnum, 
            int bucketMax, double *train_to_est_ratio);
+
+/* beta*/
 extern void CTss(int n, double *y[], double *value, double *tr_mean, double *con_mean, double *risk, 
                  double *wt, double *treatment, double max_y, double alpha, double train_to_est_ratio);
 extern void CT(int n, double *y[], double *x, int nclass,int edge, double *improve, double *split, 
                int *csplit, double myrisk, double *wt, double *treatment, int minsize, double alpha,
                double train_to_est_ratio);
+
+
 extern double CTpred(double *y, double wt, double treatment, double *yhat, double propensity);
 
 extern int CTDinit(int n, double *y[], int maxcat, char **error, int *size,

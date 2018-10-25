fa9843f26767c45c474c22be4c26029fc8a1f072
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-24 23:02:03 -0400

    Update bsplit.c

--- src/bsplit.c
+++ src/bsplit.c
@@ -70,7 +70,7 @@ bsplit(pNode me, int n1, int n2, int minsize, int split_Rule, double alpha, int 
         } else if (split_Rule == 2) {
             //CT
             (*ct_choose) (k, ytemp, xtemp, nc, ct.min_node, &improve, 
-             &split, ct.csplit, me->risk, wtemp, trtemp, minsize, alpha, train_to_est_ratio);
+             &split, ct.csplit, me->risk, wtemp, trtemp, IVtemp, minsize, alpha, train_to_est_ratio);
         } else if (split_Rule == 3) {
             //fit
             (*ct_choose) (k, ytemp, xtemp, nc, ct.min_node, &improve, 

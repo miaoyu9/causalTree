cb96e481e984da296158beba44b5702ac252a459
miaoyu9 <41392510+miaoyu9@users.noreply.github.com>
2018-10-24 22:59:02 -0400

    Update causalTree.c

--- src/causalTree.c
+++ src/causalTree.c
@@ -178,6 +178,7 @@ causalTree(SEXP ncat2, SEXP split_Rule2, SEXP bucketnum2, SEXP bucketMax2, SEXP 
     ct.numcat = INTEGER(ncat2);
     ct.wt = wt;
     ct.treatment = treatment;
+            ct.IV = IV; /* instrument */
     ct.iscale = 0.0;
     ct.vcost = REAL(cost2);
     
